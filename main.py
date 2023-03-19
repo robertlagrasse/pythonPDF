a00d25301595b4ba1e2c7af90571f20f2d95c093
robert <robert.lagrasse@gmail.com>
2023-03-19 16:46:35 -0500

    Reads topics and page counts from a list. Creates headers and footers, with the number of pages as specificed in csv. outputs to csv

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
+from fpdf import FPDF
+import pandas
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+pdf = FPDF(orientation="p", unit="mm", format="letter")
+pdf.set_auto_page_break(auto=False, margin=0)
+
+df = pandas.read_csv('data/pdf-topics.csv')
+pageCount = 0
+for index, row in df.iterrows():
+    Topic = row['Topic']
+    pages = int(row['Pages'])
+
+    for i in range(pages):
+        pageCount += 1
+        pdf.add_page()
+        pdf.set_font(family="Times", size=24)
+        pdf.set_text_color(25,25,25)
+        pdf.cell(w=0, h=24, txt=f"Topic: {Topic}",
+                 align="L", ln=1, border=0)
+        pdf.line(10, 30, 200, 30)
+        pdf.ln(220)
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(2, 2, 2)
+        pdf.cell(w=0, h=24, txt=f"Page: {pageCount}",
+                 align="R", ln=1, border=0)
+        pdf.line(10, 260, 200, 260)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+pdf.output("output.pdf")
